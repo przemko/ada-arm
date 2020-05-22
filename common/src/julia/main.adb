2bd0adec3f58e1bb85495916004545a967374aee
Przemyslaw Kobylanski <przemko@mac.com>
2020-05-22 14:21:17 +0200

    dodano przykład Julia

---
+++ common/src/julia/main.adb
@@ -0,0 +1,77 @@
+with Ada.Numerics.Generic_Complex_Types;
+with Ada.Numerics.Generic_Complex_Elementary_Functions;
+
+with Last_Chance_Handler; pragma Unreferenced (Last_Chance_Handler);
+with Interfaces; use Interfaces;
+with HAL; use HAL;
+with STM32.Board; use STM32.Board;
+with HAL.Bitmap; use HAL.Bitmap;
+with HAL.Framebuffer; use HAL.Framebuffer;
+with STM32.RNG.Polling; use STM32.RNG.Polling;
+
+procedure Main is
+
+   package Complex_Types is
+     new Ada.Numerics.Generic_Complex_Types (Float);
+   use Complex_Types;
+
+   package Complex_Functions is
+     new Ada.Numerics.Generic_Complex_Elementary_Functions (Complex_Types);
+   use Complex_Functions;
+
+   Format : constant HAL.Framebuffer.FB_Color_Mode := RGB_565;
+   White_Color : constant UInt32 := 16#FFFF#;
+   Blue_Color : constant UInt32 := 16#001F#;
+   Black_Color : constant UInt32 := 16#0000#;
+
+   LCD_W : constant := (if LCD_Natural_Width > LCD_Natural_Height
+                        then LCD_Natural_Width else LCD_Natural_Height);
+   LCD_H : constant := (if LCD_Natural_Width > LCD_Natural_Height
+                        then LCD_Natural_Height else LCD_Natural_Width);
+
+   procedure Clear;
+   procedure Step;
+
+   procedure Clear is
+   begin
+      for Y in 0 .. LCD_H loop
+         for X in 0 .. LCD_W loop
+            Display.Hidden_Buffer (1).Set_Pixel ((X, Y), Blue_Color);
+         end loop;
+      end loop;
+   end Clear;
+
+   C : Complex_Types.Complex := (Re => -0.512511498387847167,
+                                 Im =>  0.521295573094847167);
+
+   Current : Complex_Types.Complex := (Re => 0.0, Im => 0.0);
+
+   procedure Step is
+      X : Integer := Integer (Float (LCD_W) * (Current.Re + 1.5) / 3.0);
+      Y : Integer := Integer (Float (LCD_H) * (1.0 - Current.Im) / 2.0);
+   begin
+      if X >= 0 and then X < LCD_W and then Y >= 0 and then Y < LCD_H then
+         Display.Hidden_Buffer (1).Set_Pixel ((X, Y), Black_Color);
+         Display.Update_Layer (1, Copy_Back => True);
+      end if;
+
+      Current := Sqrt (Current - C);
+      if Random mod 2 = 0 then
+         Current := - Current;
+      end if;
+
+      if Random mod 1_000 = 0 then
+         Current := (0.0, 0.0);
+      end if;
+   end Step;
+
+begin
+   Initialize_RNG;
+   Display.Initialize (Orientation => Landscape);
+   Display.Initialize_Layer (1, Format);
+   Clear;
+   loop
+      Step;
+   end loop;
+end Main;
+
