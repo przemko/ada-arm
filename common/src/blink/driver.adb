133fb29de6fb70ada9bd8140a08512c34605e6a9
Przemyslaw Kobylanski <przemko@mac.com>
2020-06-03 14:49:08 +0200

    wprowadzono typ zadaniowy

--- common/src/blink/driver.adb
+++ common/src/blink/driver.adb
@@ -3,50 +3,18 @@ with Ada.Real_Time;     use Ada.Real_Time;
 
 package body Driver is
 
-   task body Controller_1 is
-      Period       : constant Time_Span := Milliseconds (75);  -- arbitrary
+   task body Controller is
+      LED : User_LED :=
+        (if C = Red then Red_LED else (if C = Green then Green_LED else Blue_LED));
+      Period : Time_Span := Milliseconds (P);
       Next_Release : Time := Clock;
-
    begin
       Initialize_LEDs;
-
       loop
-         Toggle (Red_LED);
-
+         Toggle (LED);
          Next_Release := Next_Release + Period;
          delay until Next_Release;
       end loop;
-   end Controller_1;
-
-   task body Controller_2 is
-      Period       : constant Time_Span := Milliseconds (150);  -- arbitrary
-      Next_Release : Time := Clock;
-
-   begin
-      Initialize_LEDs;
-
-      loop
-         Toggle (Blue_LED);
-
-         Next_Release := Next_Release + Period;
-         delay until Next_Release;
-      end loop;
-   end Controller_2;
-
-
-   task body Controller_3 is
-      Period       : constant Time_Span := Milliseconds (300);  -- arbitrary
-      Next_Release : Time := Clock;
-
-   begin
-      Initialize_LEDs;
-
-      loop
-         Toggle (Green_LED);
-
-         Next_Release := Next_Release + Period;
-         delay until Next_Release;
-      end loop;
-   end Controller_3;
+   end Controller;
 
 end Driver;
--- common/src/blink/driver.ads
+++ common/src/blink/driver.ads
@@ -1,12 +1,15 @@
+with Ada.Real_Time; use Ada.Real_Time;
+with STM32.Board; use STM32.Board;
+
 package Driver is
 
-   task Controller_1 with
+   type Colour is (Red, Green, Blue);
+
+   task type Controller (P  : Positive := 100; C : Colour := Red) with
      Storage_Size => (4 * 1024);
 
-   task Controller_2 with
-     Storage_Size => (4 * 1024);
-
-   task Controller_3 with
-     Storage_Size => (4 * 1024);
+   C1 : Controller ( 75, Red  );
+   C2 : Controller (150, Blue );
+   C3 : Controller (300, Green);
 
 end Driver;
