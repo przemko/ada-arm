55d06e94ba5cc2e595516f2616b1126b246329cf
Przemyslaw Kobylanski <przemko@mac.com>
2020-06-22 08:20:35 +0200

    dodano figurę king

--- common/src/chess/board.adb
+++ common/src/chess/board.adb
@@ -54,7 +54,7 @@ package body Board is
       16#007ffe00#, 16#003ffc00#, 16#0ffffff0#, 16#1ffc3ff8#,
       16#00000000#, 16#00000000#, 16#00000000#, 16#00000000#);
 
-   Queen_Bitmap : Piece_Bitmap :=
+   Queen_Bitmap : Piece_Bitmap :=               -- poprawic bo brzydko wyglada
      (16#00000000#, 16#00000000#, 16#00000000#, 16#00018000#,
       16#0063c600#, 16#00f3cf00#, 16#0cf18f30#, 16#1e618678#,
       16#1e63c678#, 16#0c63c630#, 16#0cf3cf30#, 16#0cf7ef30#,
@@ -63,6 +63,16 @@ package body Board is
       16#021ff840#, 16#03ffffc0#, 16#030000c0#, 16#07ffffe0#,
       16#0e1ff870#, 16#1fc3c3f8#, 16#1ff81ff8#, 16#07ffffe0#,
       16#007ffe00#, 16#00000000#, 16#00000000#, 16#00000000#);
+
+   King_Bitmap : Piece_Bitmap :=
+     (16#00000000#, 16#00000000#, 16#00018000#, 16#00018000#,
+      16#00018000#, 16#0007e000#, 16#0007e000#, 16#00018000#,
+      16#0e018070#, 16#1f03c0f8#, 16#3f87e3fc#, 16#7fcff3fe#,
+      16#7fc7e3fe#, 16#7fe3c7fe#, 16#7ff3cffe#, 16#7ffffffe#,
+      16#7ffffffe#, 16#7ffffffe#, 16#3ffffffc#, 16#1ffffff8#,
+      16#0ffffff0#, 16#0ffffff0#, 16#07ffffe0#, 16#07ffffe0#,
+      16#06aa5560#, 16#0ffffff0#, 16#1ffffff8#, 16#1ffffff8#,
+      16#00000000#, 16#00000000#, 16#00000000#, 16#00000000#);
 
    procedure Clear (Column : Column_Type; Row : Row_Type) is
    begin
@@ -155,8 +165,14 @@ package body Board is
                end loop;
             end loop;
 
-         when others =>
-            null;
+         when King =>
+            for I in 0..31 loop
+               for J in 0..31 loop
+                  if (King_Bitmap(J) and 2**I) /= 0 then
+                     Display.Hidden_Buffer (1).Set_Pixel ((X+I, Y+J), PixCol);
+                  end if;
+               end loop;
+            end loop;
       end case;
 
       Display.Update_Layer (1, Copy_Back => True);
